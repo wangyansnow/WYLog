0a2efd0f3d1d393d36138820c540bd7030f39e34
wangyansnow <wangyan6@cmcm.com>
2018-03-02 20:12:01 +0800

    增加OC用户，去掉冗余代码

--- WYLog/ViewController.swift
+++ WYLog/ViewController.swift
@@ -12,22 +12,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (_) in
-            var i = 2
-        }
+
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
-//        WYLogUtil.wy_log("有没有人曾告诉你")
-//        WYLogUtil.wy_log("wy_min", isWriteToFile: true)
-//        WYLogUtil.wy_log("dsfdsafjewoiqpwrekjqprjkwqjr32")
-//        WYLogUtil.wy_log("有dsfjewqlijwlirji23人曾告诉你")
-//        WYLogUtil.wy_log("有没dsfkewqrk3q;2okr;32mlirj32li有人曾告诉你")
-//        WYPerson.log()
-        
         for i in 0..<1000 {
+            WYPerson.log()
             DispatchQueue.global().async {
                 WYLogUtil.wy_log("i = \(i) ====== ")
             }
